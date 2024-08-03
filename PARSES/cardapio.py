ac21dfca300464946e123ad18cfc478c76be198e
poliana-dev <m.polisys02@gmail.com>
2024-08-03 13:02:21 -0300

    feat: parse cardapio

--- PARSES/cardapio.py
+++ PARSES/cardapio.py
@@ -3,53 +3,36 @@ from xml.dom.minidom import parse
 # determio o caminho do arquivo que desejo
 caminho = parse("./XML_DTD/atividade01/cardapio.xml")
 
+
 # 'crio" a raiz
 cardapio = caminho.documentElement
 pratos= cardapio.getElementsByTagName('prato') #pego o elemento apartir da raiz
 
 print("\nSegue a lista dos nossos pratos:\n")
 
-id_ref=0
+# determino uma lista
+nomes= []
 for prato in pratos:
     id = prato.getAttribute('id')
     nome = prato.getElementsByTagName('nome')[0].firstChild.nodeValue
-    # descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
-    # ingredientes = prato.getElementsByTagName('ingredientes')[0].firstChild.nodeValue 
-    # preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
-    # calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
-    # tempoP = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
-    id_ref = id_ref+1
+    nomes.append(prato.getElementsByTagName('nome')[0].firstChild.nodeValue) #cada nome é add
+
 
     print(f"{id} - {nome}")
 
 print("-"*30)
-campo= int(input("informe o indice:"))
-prato = pratos[campo-1]
-descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
-ingredientes = prato.getElementsByTagName('ingredientes')[0].firstChild.nodeValue 
-preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
-calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
-tempoP = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
+campo= int(input("informe o indice: "))
+print("\n")
 
-print(descricao)
+# se campo é menor ou igual que a lista dos pratos e dos nome
+if  campo <= len(nomes) and 1<=campo<= len(pratos):
+    nome = nomes[campo-1] # as listas começam com zero, portanto [campo-1]
+    prato = pratos[campo-1]
+    descricao = prato.getElementsByTagName('descricao')[0].firstChild.nodeValue
+    ingredientes = prato.getElementsByTagName('ingrediente')[0].firstChild.nodeValue 
+    preco = prato.getElementsByTagName('preco')[0].firstChild.nodeValue
+    calorias = prato.getElementsByTagName('calorias')[0].firstChild.nodeValue
+    tempoP = prato.getElementsByTagName('tempoPreparo')[0].firstChild.nodeValue
 
-# for prato in pratos:
-#     if (campo == id_ref):
-#       
-#         
-#         print(descricao)
-
-
-
-
-
-
-    
-
-
-
-
-
-
-# print("Olá, bem-vindo(a)!\n Segue a lista dos nossos pratos:")
-# pergunta= input(str("Digite o número correspondente ao prato desejado:"))
+    print(f"Prato escolhido: {nome}")
+    print(f"Descrição: {descricao}\nIngredientes:\n     {ingredientes}\nPreço: {preco}\nCalorias: {calorias}\nTempo de preparo: {tempoP} ")
