cbce54204b8669b8656900ed5eac94b34237ff90
poliana-dev <m.polisys02@gmail.com>
2024-08-07 21:51:29 -0300

    feat: list e dict mas ainda não criou um dicionario propriamente dito.

--- PARSES/imobiliaria.py
+++ PARSES/imobiliaria.py
@@ -1,9 +1,6 @@
 from xml.dom.minidom import parse
 import json
 
-
-#fazer o mesmo que cardapio mas ter um dict vazio e uma lista vazia.. e depois adcionar as coisas do dict na lista
-# e depois fazer as funçoes do json
 caminho = parse("./XMLSchema/mobiliaria/imobiliaria.xml")
 
 #raiz do xml
@@ -11,14 +8,41 @@ imobiliaria = caminho.documentElement
 imoveis= imobiliaria.getElementsByTagName('imovel')
 
 imobiliaria=[]
+moveis = {}
 
 
 for imovel in imoveis:
     descricao = imovel.getElementsByTagName('descricao')[0].firstChild.nodeValue
-    proprietario = imovel.getElementsByTagName('proprietario')[0].firstChild.nodeValue
-    endereco = imovel.getElementsByTagName('endereco')[0].firstChild.nodeValue
+    nome_prop = imovel.getElementsByTagName('nome')[0].firstChild.nodeValue
+    email_prop =  imovel.getElementsByTagName('email')[0].firstChild.nodeValue
+    telefone_prop = imovel.getElementsByTagName('telefone')[0].firstChild.nodeValue
 
+    # endereço
+    rua = imovel.getElementsByTagName('rua')[0].firstChild.nodeValue
+    bairro = imovel.getElementsByTagName('bairro')[0].firstChild.nodeValue
+    cidade = imovel.getElementsByTagName('cidade')[0].firstChild.nodeValue
 
+    numQuartos = imovel.getElementsByTagName('numQuartos')[0].firstChild.nodeValue
+    numBanheiros = imovel.getElementsByTagName('numBanheiros')[0].firstChild.nodeValue
+    tamanho = imovel.getElementsByTagName('tamanho')[0].firstChild.nodeValue
 
+    valor = imovel.getElementsByTagName('valor')[0].firstChild.nodeValue
 
-#imobilibiaria é a lista de imoveis e os imoveis o dict
+    imobiliaria.append(descricao)
+    imobiliaria.append(nome_prop)
+    imobiliaria.append(email_prop)
+    imobiliaria.append(telefone_prop)
+    imobiliaria.append(rua)
+    imobiliaria.append(bairro)
+    imobiliaria.append(cidade)
+    imobiliaria.append(numQuartos)
+    imobiliaria.append(numBanheiros)
+    imobiliaria.append(tamanho)
+    imobiliaria.append(valor)
+    
+    moveis= imobiliaria
+
+# with open ('imobiliaria.json', 'w') as json_file:
+#     json.dump(moveis, json_file,indent=2)
+
+# json_string = json.dumps(moveis)
