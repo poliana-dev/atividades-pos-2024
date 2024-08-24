e9fcac99b172f011d7473f867f405de5c1316c28
poliana-dev <m.polisys02@gmail.com>
2024-08-23 22:20:19 -0300

    feat: deixar de seguir user

--- AUTH/senha.py
+++ AUTH/senha.py
@@ -2,9 +2,6 @@ import requests
 from requests.auth import HTTPBasicAuth
 from getpass import getpass
 
-
-
-
 user = input("Informe seu nome de usuário do git: ")
 password = input("Informe seu token/senha de usuário do git: ")
 print("-"*30)
@@ -16,26 +13,36 @@ if op ==1:
     response = requests.get(f'https://api.github.com/users/{user}/followers',
                 auth = HTTPBasicAuth('user', password))
     
-    for seguidor in response.json():
-        print(seguidor['login'])
+    
 
+    if response.status_code == 200:
+        for seguidor in response.json():
+            print(seguidor['login'])
+    else: 
+        print("Algo de errado aconteceu.")
+
+
+#para seguir
 elif op == 2:
     username = input("Digite o nome do usuário que você deseja seguir: ")
     response = requests.put(f'https://api.github.com/user/following/{username}',
                 auth = HTTPBasicAuth(user, password))
-
-
-
-
-# user = input("para conhecer os usuarios de um user: ")
-
-# response = requests.get(f'https://api.github.com/users/{user}/followers',
-#             auth = HTTPBasicAuth('user', password))
-
-# for seguidor in response.json():
-#     print(seguidor['login'])
-
-print(response.status_code)
+    
+    if response.status_code == 204:
+        print(f"Agora você está seguindo: {username}")
+    else:
+        print("Algo de errado aconteceu.")
+    
+elif op == 3:
+    username = input("Digite o nome do usuário que você QUER DEIXAR de seguir: ")
+    response = requests.delete(f'https://api.github.com/user/following/{username}',
+                auth = HTTPBasicAuth(user, password))
+    
+    if response.status_code == 204:
+        print(f"Você deixou de seguir: {username}")
+    else:
+        print("Algo de errado aconteceu.")
+    
 
 
 
